e63b7de9c42f8ed916a11e5f4b6297752f0f52a6
Alex Rybachev <rybachev.as@gmail.com>
2021-06-09 15:50:14 +0400

    Initial Commit

---
+++ MyStateAndDataFlow/MyStateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStateAndDataFlowApp.swift
+//  MyStateAndDataFlow
+//
+//  Created by Aleksandr Rybachev on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyStateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

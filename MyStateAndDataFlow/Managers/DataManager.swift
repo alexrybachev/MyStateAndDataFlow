42c5634665c4d7075c1af41d1e07a09c84844e74
Alex Rybachev <rybachev.as@gmail.com>
2021-06-12 10:04:02 +0400

    AppStorage

--- MyStateAndDataFlow/Managers/DataManager.swift
+++ MyStateAndDataFlow/Managers/DataManager.swift
@@ -5,32 +5,29 @@
 //  Created by Aleksandr Rybachev on 10.06.2021.
 //
 
-import Foundation
+import SwiftUI
 
 class DataManager {
+    
     static let shared = DataManager()
     
-    private let userDefaults = UserDefaults.standard
-    private let key = "userManager"
+    @AppStorage("user") private var userData: Data?
     
     private init() {}
     
     func saveUser(user: User) {
-        guard let userData = try? JSONEncoder().encode(user) else { return }
-        userDefaults.set(userData, forKey: key)
+        userData = try? JSONEncoder().encode(user)
     }
     
     func loadUser() -> User {
-        guard let userData = userDefaults.object(forKey: key) as? Data else { return User() }
-        guard let user = try? JSONDecoder().decode(User.self, from: userData) else { return User() }
+        guard let user = try? JSONDecoder().decode(User.self, from: userData ?? Data()) else { return User() }
         return user
     }
     
     func deleteUser(userManager: UserManager) {
         userManager.user.isRegistered = false
         userManager.user.name = ""
-        userDefaults.removeObject(forKey: key)
+        userData = nil
     }
-    
 }
 
